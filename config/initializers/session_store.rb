d8f1098fa2f70b643d3a12cfb4bd791359f64158
bsorbus <bsorbus@gmail.com>
2023-03-10 15:01:23 +0100

    Add force_ssl & secure: true

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -8,5 +8,6 @@ Rails.application.config.session_store :redis_store, {
   ],
   expire_after: 60.minutes,
   key: Rails.application.secrets.cache_store_key,
-  same_site: :lax
+  same_site: :lax,
+  secure: true
 }
